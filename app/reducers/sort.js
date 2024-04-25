f7d20aef676ae87e86a6db405c1454f8374e2391
Maria Torrente <64274966+mariatorrentedev@users.noreply.github.com>
2024-04-25 10:00:15 -0400

    Customer 2 (#2)
    * customer-2: fixing state in reducer.
    
    * customer-2: minor clean up.

--- app/reducers/sort.js
+++ app/reducers/sort.js
@@ -1,15 +1,17 @@
-import { ChangeSort } from '../actions/sort';
+import { ChangeSort } from "../actions/sort";
 
 const InitialState = {
-  sort: 'old',
+  sort: "old",
 };
 
 export default function posts(state = InitialState, action) {
   switch (action.type) {
-    case ChangeSort: {
-      state.sort = action.sort;
-      return state;
-    }
+    // Update the sort parameter in the state based on the ChangeSort action.
+    case ChangeSort:
+      return {
+        ...state,
+        sort: action.sort,
+      };
 
     default:
       return state;
